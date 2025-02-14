75d5a4f3c62207c3213078810e62fcfa51b3fca8
HugoZAuad <hugozeymer@hotmail.com>
2025-02-13 22:22:36 -0300

    Feito ajustes no contato

--- src/view/Contato/Contato.tsx
+++ src/view/Contato/Contato.tsx
@@ -4,19 +4,18 @@ import "./Contato.css";
 interface ContactCardProps {
   name: string;
   email: string;
-  phone: string;
   message: string;
 }
 
-const ContactCard: React.FC<ContactCardProps> = ({ name, email, phone, message }) => {
+const ContactCard: React.FC<ContactCardProps> = ({ name, email, message }) => {
   return (
     <div className="contact-card">
       <h2>Informações de Contato</h2>
       <div className="info-group">
-        <strong>Nome:</strong> <span>HUGO ZEYMER AUAD</span>
+        <strong>Nome:</strong> <span>{name}</span>
       </div>
       <div className="info-group">
-        <strong>E-mail:</strong> <span>hugozeymer@gmail.com</span>
+        <strong>E-mail:</strong> <span>{email}</span>
       </div>
       <div className="info-group">
         <strong>Linktree:</strong> <span>{message}</span>
