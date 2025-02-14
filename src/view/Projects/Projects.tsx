8765dd02e08e33d8f1fbc9c12eaf855a9e521eff
HugoZAuad <hugozeymer@hotmail.com>
2025-02-14 13:53:29 -0300

    Mudanças nos projetos, cards acertados, e feito transição por projeto

--- src/view/Projects/Projects.tsx
+++ src/view/Projects/Projects.tsx
@@ -4,7 +4,7 @@ import "./Projects.scss";
 
 const CardFlip: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const cardsToShow = 5; // Número de cards a serem exibidos
+  const cardsToShow = 5;
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
@@ -14,7 +14,6 @@ const CardFlip: React.FC = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + data.length) % data.length);
   };
 
-  // Calcula os índices dos cards a serem exibidos
   const displayedCards = data.slice(currentIndex, currentIndex + cardsToShow).concat(
     data.slice(0, Math.max(0, currentIndex + cardsToShow - data.length))
   );
@@ -45,10 +44,11 @@ const Card: React.FC<{ item: { title: string; description: string; image: string
       <div className="card-front">
         <img src={item.image} alt={item.title} />
         <h3>{item.title}</h3>
+        <p>Clique para ver os detalhes</p>
       </div>
       <div className="card-back">
         <p>{item.description}</p>
-        <a href={item.link} target="_blank" rel="noopener noreferrer">Saiba mais</a>
+        <a href={item.link} target="_blank" rel="noopener noreferrer">Repositorio do projeto</a>
       </div>
     </div>
   );
